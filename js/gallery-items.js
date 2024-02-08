d166c3ba9d85995c6a69ba4e1bbc6c108ba70b5d
achoma <139071702+achoma@users.noreply.github.com>
2024-02-08 23:20:38 +0100

    update

---
+++ js/gallery-items.js
@@ -0,0 +1,66 @@
+export const galleryItems = [
+  {
+    preview:
+      './images/portfolio1.png',
+    original:
+      './images/portfolio1.png',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+    './images/portfolio2.png',
+    original:
+    './images/portfolio2.png',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+    './images/portfolio3.png',
+    original:
+    './images/portfolio3.png',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+    './images/portfolio4.png',
+    original:
+    './images/portfolio4.png',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+    './images/portfolio5.png',
+    original:
+    './images/portfolio5.png',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+    './images/portfolio6.png',
+    original:
+    './images/portfolio6.png',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+    './images/portfolio7.png',
+    original:
+    './images/portfolio7.png',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+    './images/portfolio8.png',
+    original:
+    './images/portfolio8.png',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+    './images/portfolio5.png',
+    original:
+    './images/portfolio5.png',
+    description: 'Nature Landscape',
+  },
+  
+];
---
+++ js/index.js
@@ -0,0 +1,17 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const gallery = document.querySelector(".gallery")
+const images = galleryItems.map(({preview, original, description}) => {
+    return `<li>
+        <div class="gallery__item">
+            <a class="gallery__item" href="${original}">
+               <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </div> </li>`})        
+        
+        gallery.insertAdjacentHTML(`beforeend`, images.join(""))
+        
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
