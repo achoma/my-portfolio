e05cc59c0d18805520a8ce0df9e0a1a91d6b82b2
achoma <139071702+achoma@users.noreply.github.com>
2024-02-20 01:35:10 +0100

    form

--- js/server.js
+++ js/server.js
@@ -1,19 +1,23 @@
-document.getElementById('#contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Zatrzymaj domyślne zachowanie formularza
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Zatrzymaj domyślne zachowanie formularza
+      let formData = new FormData(this);
   
-    let formData = new FormData(this);
-  
-    fetch('mailto:aga.choma02@gmail.com', {
-      method: 'POST',
-      body: formData
-    }).then(function(response) {
-      if (response.ok) {
-        alert('Dane zostały wysłane pomyślnie!');
-        this.reset();
-      } else {
-        alert('Wystąpił problem podczas wysyłania danych.');
-      }
-    }).catch(function(error) {
-      alert('Wystąpił problem podczas wysyłania danych: ' + error.message);
+      fetch(this.action, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          alert('Dane zostały wysłane pomyślnie!');
+          this.reset();
+        } else {
+          alert('Wystąpił problem podczas wysyłania danych.');
+        }
+      }).catch(error => {
+        alert('Wystąpił problem podczas wysyłania danych: ' + error.message);
+      });
     });
   });
