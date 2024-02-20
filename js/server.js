f9c33526f40bd777538cece2efad2b6366518909
achoma <139071702+achoma@users.noreply.github.com>
2024-02-20 15:32:35 +0100

    form update

--- js/server.js
+++ js/server.js
@@ -1,23 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('contactForm').addEventListener('submit', function(event) {
-      event.preventDefault(); // Zatrzymaj domyślne zachowanie formularza
-      let formData = new FormData(this);
-  
-      fetch(this.action, {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          alert('Dane zostały wysłane pomyślnie!');
-          this.reset();
-        } else {
-          alert('Wystąpił problem podczas wysyłania danych.');
-        }
-      }).catch(error => {
-        alert('Wystąpił problem podczas wysyłania danych: ' + error.message);
-      });
-    });
-  });
+const form = document.getElementById('contactForm');
+
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(form);
+            const response = await fetch(form.action, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                alert('Your message has been sent successfully!');
+                form.reset();
+            } else {
+                alert('Oops! There was a problem sending your message. Please try again later.');
+            }
+        });
